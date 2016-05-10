1bfea1a665c95e062eeaf2ed95dec7bd49602ef2
Mychel Rennó <mychelrenno@gmail.com>
2016-05-09 22:19:22 -0300

    crude cliente e produto

--- src/java/br/com/ecommerce/dao/ProdutoDAO.java
+++ src/java/br/com/ecommerce/dao/ProdutoDAO.java
@@ -5,10 +5,69 @@
  */
 package br.com.ecommerce.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mychel Rennó
  */
 public class ProdutoDAO {
     
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    
+    public ProdutoDAO(){
+        
+    }
+    
+    public void create(Object object) {
+        connection = new Conexao().getConnection();
+        
+        try {
+            PreparedStatement ps = connection
+                    .prepareStatement("INSERT INTO PESSOA (ID, NOME) VALUES (?, ?)");
+            ps.setInt(1, 1);
+            ps.setString(2, "Hugo");
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void read(Object object) {
+        try {
+            try {
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery("SELECT * FROM PESSOA");
+                while (resultSet.next()) {
+                        int id = resultSet.getInt(1);
+                        String nome = resultSet.getString("nome");
+                        System.out.println(id + " " + nome);
+                }
+            } finally {
+                if (statement != null) statement.close();
+                if (resultSet != null) resultSet.close();
+            }
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+    }
+
+    private void update(Object object) {
+
+            String sql = "UPDATE PESSOA SET ID = ?, NOME = ? WHERE ID = ?";
+    }
+
+    private void delete(Object object) {
+
+            String sql = "DELETE FROM PESSOA WHERE ID = ?";
+
+    }
 }
